47ac680196cb2f37052945c5b005566016d1aeb2
Alberto Del Buono Paolini <alberto.delbuonopaolini@stud.unifi.it>
2022-06-11 21:05:51 +0200

    Removed memory leaks

--- app/src/main/java/com/termux/MainActivity.java
+++ app/src/main/java/com/termux/MainActivity.java
@@ -23,12 +23,12 @@ import java.io.File;
 
 public class MainActivity extends Activity {
 
-    public static MainActivity activity;
+    public MainActivity activity;
     public AlertDialog alertDialog;
-    public static TextView text_welcome;
+    public TextView text_welcome;
     public Button btnConsole;
-    public static Button btnNodeRed;
-    public static Button btnDashBoard;
+    public Button btnNodeRed;
+    public Button btnDashBoard;
     public Button btnInfo;
     private Intent info_intent;
     public static boolean enableNodeRed = false;
@@ -76,7 +76,7 @@ public class MainActivity extends Activity {
             try {
                 Log.d("termux", "refreshing  Node-dashobard");
 
-                MainActivity.btnDashBoard.refreshDrawableState();
+                btnDashBoard.refreshDrawableState();
             } catch (Exception e) {
                 Log.d("termux", "exception from enable btn-dashboard:" + e.getMessage());
             }
